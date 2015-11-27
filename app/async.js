0338e0d27cc1067eebc0f143549c8529019519dd
Gerald Tilma <gerald.tilma@gmail.com>
2015-11-27 12:42:57 -0600

    Implement async functions

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
+    var deferred = $.Deferred();
 
+    setTimeout(function() {
+      deferred.resolve(value);
+    }, 1);
+
+    return deferred.promise(); 
   },
 
   manipulateRemoteData : function(url) {
+    var deferred = $.Deferred();
 
+    $.ajax(url).then(function(response) {
+      var results = response.people.map(function(item) {
+        return person.name;
+      }).sort();
+
+      deferred.resolve(results);
+    });
+
+    return deferred.promise();
   }
 };

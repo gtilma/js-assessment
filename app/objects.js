64c2a3cc4c8422b2e36915e4ceb6410ada49313a
Gerald Tilma <gerald.tilma@gmail.com>
2015-11-27 18:28:47 -0600

    Implement object functions

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,18 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    return constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
+    var keys = Object.keys(obj);
 
+    return keys.map(function(key) {
+      return key + ': ' + obj[key];
+    });
   }
 };

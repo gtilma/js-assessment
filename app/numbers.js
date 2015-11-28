a00c9db0710cc5bc8ec13a605bd12968fc42b554
Gerald Tilma <gerald.tilma@gmail.com>
2015-11-27 18:17:55 -0600

    Implement number functions

--- app/numbers.js
+++ app/numbers.js
@@ -2,15 +2,22 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    var b = num.toString(2);
+    return parseInt(b[b.length-bit]);
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
+    var b = num.toString(2);
 
+    while (b.length < 8) {
+      b = '0' + b;
+    }
+
+    return b;
   },
 
   multiply: function(a, b) {
